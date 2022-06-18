0fd5d20141b84c5d589f192e60ded000b76ad12f
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-18 17:47:27 +0200

    Code implementation

--- javascript/_imgGallery/script.js
+++ javascript/_imgGallery/script.js
@@ -1,3 +1,31 @@
+let currentySelected = 0;
+let images = document.querySelectorAll('.gallery-img');
+const btnPrevious = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
+
+btnPrevious.addEventListener('click', function () {
+
+    images[currentySelected].classList.remove("active");
+    currentySelected--;
+    images[currentySelected].classList.add("active");
+
+    btnNext.removeAttribute("disabled");
+    if (currentySelected === 0) {
+        btnPrevious.setAttribute("disabled", true);
+    }
+})
+
+btnNext.addEventListener('click', function () {
+
+    images[currentySelected].classList.remove("active");
+    currentySelected++;
+    images[currentySelected].classList.add("active");
+
+    btnPrevious.removeAttribute("disabled");
+    if (currentySelected === images.length - 1) {
+        btnNext.setAttribute("disabled", true);
+    }
+});
 
 
 
