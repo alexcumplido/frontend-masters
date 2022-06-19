9d730a7226050b104109eeb61cb3744ca1caecd0
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-19 08:28:15 +0200

    Swiper implemented

---
+++ javascript/_imgSwiper/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 800,
+    spaceBetween: 400,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
