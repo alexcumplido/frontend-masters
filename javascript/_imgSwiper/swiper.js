6bcdc71bee4a67c19798e7c5fd8a6d3a43ca401d
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-20 10:27:38 +0200

    Implemented Popmotion Library via cdn

--- javascript/_imgSwiper/swiper.js
+++ javascript/_imgSwiper/swiper.js
@@ -1,12 +1,13 @@
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-    speed: 800,
-    spaceBetween: 400,
+    speed: 400,
+    spaceBetween: 50,
+    effect: "flip",
+    slidesPerView: 2,
 
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-});
+}); 
