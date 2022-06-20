a1d3e4224a2dd00dbb7a19d1e6c69b8e5ab6cd9b
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-20 15:52:03 +0200

    Dog AJAX fetch implemented

--- javascript/project-dogs/script.js
+++ javascript/project-dogs/script.js
@@ -1,4 +1,28 @@
+const dogTarget = document.querySelector('.dog-target');
+const buttonDog = document.querySelector('.dog-btn');
 
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+function fetchDog() {
+
+    fetch(DOG_URL)
+        .then(function (response) {
+            const processingPromise = response.text();
+            return processingPromise;
+        })
+        .then(function (processedResponse) {
+            const dogObj = JSON.parse(processedResponse);
+            const img = document.createElement('img');
+            img.src = dogObj.message;
+            img.alt = "Cute doggo";
+            dogTarget.appendChild(img);
+        });
+}
+
+buttonDog.addEventListener('click', function (event) {
+    fetchDog();
+    console.log("Dog fetched");
+});
 
 
 
