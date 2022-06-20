cc7e6bd87db8ba41a41e9447e765c84a530366cd
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-20 18:47:22 +0200

    Refactored fetch breeds

--- javascript/project-dogs/script.js
+++ javascript/project-dogs/script.js
@@ -1,23 +1,26 @@
 const URL_BREED_DOGS = "https://dog.ceo/api/breeds/list/all";
-let breedSelect = document.querySelector(".breed-select");
+let select = document.querySelector(".breed-select");
 
 function fetchBreeds() {
     fetch(URL_BREED_DOGS)
         .then((response) => response.json())
-        .then((processedResponse) => {
-            const objBreed = processedResponse.message;
-            for (const breed in objBreed) {
-                console.log(breed);
+        .then((data) => {
+            const breedsObject = data.message;
+            const breedsArray = Object.keys(breedsObject);
+            breedsArray.forEach((breed) => {
                 const option = document.createElement("option");
                 option.value = breed;
                 option.innerText = breed;
-                breedSelect.appendChild(option);
-            }
+                select.appendChild(option);
+            })
         });
 }
 
-breedSelect.addEventListener('click', function () {
-    fetchBreeds();
-});
+select.addEventListener('change', function (event) {
+    console.log(event.target.value);
+
+})
+
+fetchBreeds();
 
 
