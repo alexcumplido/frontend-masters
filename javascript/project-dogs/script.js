56f014dc331cb94acff66d6b8d5bd38ebebf9780
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-20 18:25:15 +0200

    Implement fetchBreeds via eventListener into breedSelect

--- javascript/project-dogs/script.js
+++ javascript/project-dogs/script.js
@@ -1,6 +1,23 @@
+const URL_BREED_DOGS = "https://dog.ceo/api/breeds/list/all";
+let breedSelect = document.querySelector(".breed-select");
 
+function fetchBreeds() {
+    fetch(URL_BREED_DOGS)
+        .then((response) => response.json())
+        .then((processedResponse) => {
+            const objBreed = processedResponse.message;
+            for (const breed in objBreed) {
+                console.log(breed);
+                const option = document.createElement("option");
+                option.value = breed;
+                option.innerText = breed;
+                breedSelect.appendChild(option);
+            }
+        });
+}
 
-
-
+breedSelect.addEventListener('click', function () {
+    fetchBreeds();
+});
 
 
