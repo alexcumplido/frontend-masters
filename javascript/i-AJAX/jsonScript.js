e1b6ea7c99e67487c115b19cc8521ade552f4ba7
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-20 16:36:48 +0200

    Complemented exercises

--- javascript/i-AJAX/jsonScript.js
+++ javascript/i-AJAX/jsonScript.js
@@ -1,11 +1,7 @@
-const outputJSON = document.querySelector('.output-JSON')
+
 const responseFromServer = `{"name": "Luna","age": 10,"breed": "Havanese","location": {"city":"Seattle","state": "WA"}}`;
-console.log(responseFromServer);
 
 const responseObject = JSON.parse(responseFromServer);
-console.log(responseObject);
-console.log(responseObject.name);
-console.log(responseObject.location.city);
 
 const dog = {
     name: "Luna",
@@ -17,10 +13,4 @@ const dog = {
     },
 };
 
-const objString = JSON.stringify(dog, null, 6);
-console.log(objString);
-
-
-setTimeout(() => {
-    outputJSON.innerHTML = objString;
-}, "1000");
+const objString = JSON.stringify(dog, null, 2);
