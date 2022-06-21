0faacb602e600f518cf27e16dc098fb0a9fcd3a2
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-21 14:58:40 +0200

    Code formatted

--- javascript/project-dogs/script.js
+++ javascript/project-dogs/script.js
@@ -1,13 +1,12 @@
 const URL_BREED_DOGS = "https://dog.ceo/api/breeds/list/all";
 let select = document.querySelector(".breed-select");
-
 let imageDog = document.querySelector(".target-dog-img");
 let spinner = document.querySelector(".spinner");
 
 function fetchBreeds() {
     fetch(URL_BREED_DOGS)
         .then(function (response) {
-            return response.json()
+            return response.json();
         })
         .then(function (data) {
             const breedsObject = data.message;
