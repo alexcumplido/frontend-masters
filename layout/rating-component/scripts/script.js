14cda123915d33e12bc7bf9c31c0b0dd93994b6a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-22 12:01:43 +0200

    Implemented manipulation effectevly

--- layout/rating-component/scripts/script.js
+++ layout/rating-component/scripts/script.js
@@ -1,45 +1,47 @@
-// User rate via button
 
-//// Add event Listener once clicked add .class
-//// Create array for number of rates
-//// Once clicked push identifier to array
-
-//// Submit only possible if just one rate selected
-// Add event that submits only if there is one rate
-// If not display an alert message
-// Once submited display none main card and turn on thankyou
-
+const buttonsGroup = document.querySelector(".ratings__btn-group");
 const btnGroup = document.querySelectorAll(".btn-rating");
 const btnSubmit = document.querySelector(".btn-submit");
 const modalRating = document.querySelector(".rating");
 const modalRatingThankyou = document.querySelector(".rating-thankyou");
-let rates = [];
+let rate = 0;
 
-btnGroup.forEach(function (btn) {
-    btn.addEventListener('click', function (event) {
+for (let i = 0; i < btnGroup.length; i++) {
+    btnGroup[i].addEventListener('click', function (event) {
+        rateButton(event);
+    })
+}
 
-        if (rates.indexOf(event.target.innexText) === 0) {
-            event.target.classList.remove('btn-rating-active');
-            rates.pop();
-        } else {
+function rateButton(event) {
+    if (event.target.innerText) {
+        if (rate === 0) {
+            rate = parseInt(event.target.innerText);
             event.target.classList.add("btn-rating-active");
-            rates.push(Number(event.target.innerText));
+        } else {
+            btnGroup.forEach(function (button) {
+                if (parseInt(button.innerText) === rate) {
+                    button.classList.remove("btn-rating-active");
+                }
+            });
+            event.target.classList.add("btn-rating-active");
+            rate = parseInt(event.target.innerText);
         }
-
-        console.log(event);
-        console.log(rates);
-        console.log(rates.length);
+    }
+}
 
 
-    });
 
-});
 
-/*
-    1. User clicks rate button
-        If rate exist 
-            This rate is removed from array rates
-            This button
-        
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
