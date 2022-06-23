fcad56ccf6aaa0fba84c2e9e7e7d1db9ea050d57
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-23 15:13:21 +0200

    Formatted exercise instructions

--- javascript/1-getifyKyleSimpson/script.js
+++ javascript/1-getifyKyleSimpson/script.js
@@ -1,6 +1,16 @@
-// TODO: define addFavoriteBook(..) function
 
-// TODO: define printFavoriteBooks() function
+function addFavoriteBook(bookName) {
+    if (!bookName.toLowerCase().includes('great')) {
+        favoriteBooks.push(bookName);
+    }
+}
+
+function printFavoriteBooks() {
+    console.log(`Favorite Books: ${favoriteBooks.length}`);
+    for (let i = 0; i < favoriteBooks.length; i++) {
+        console.log(`Book number ${i} in list is: ${favoriteBooks[i]}`);
+    }
+}
 
 var favoriteBooks = [];
 
@@ -10,4 +20,10 @@ addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
-// TODO: print out favorite books
+printFavoriteBooks();
+
+// 1. Define an `addFavoriteBook(..)` function that receives a single parameter, called `bookName`.
+// 2. If the provided `bookName` string does *NOT* have the word "Great" in it, add it to the `favoriteBooks` array.
+// 3. Define a `printFavoriteBooks()` function that receives no parameters.
+// 4. `printFavoriteBooks()` should first print a message like "Favorite Books: ..", and include the number of books in the `favoriteBooks` array.
+// 5. Finally, `printFavoriteBooks()` should loop through the `favoriteBooks` array and print out each value.
