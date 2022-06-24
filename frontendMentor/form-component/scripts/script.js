c6f8dab82ca6aea121872e4c0432d8b81383083f
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-24 17:39:09 +0200

    Portfolio backup

--- frontendMentor/form-component/scripts/script.js
+++ frontendMentor/form-component/scripts/script.js
@@ -3,5 +3,3 @@
 // The email address is not formatted correctly
 
 // https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
-// https://developer.mozilla.org/en-US/docs/Learn/Forms/Advanced_form_styling
-// https://developer.mozilla.org/en-US/docs/Learn/Forms/UI_pseudo-classes
