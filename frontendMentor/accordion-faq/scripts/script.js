77cdf7600fd47f5ce05afe043d3f713f3e4eeccf
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-24 11:16:34 +0200

    Rotate svg via DOM

--- frontendMentor/accordion-faq/scripts/script.js
+++ frontendMentor/accordion-faq/scripts/script.js
@@ -1,18 +1,23 @@
 let accordionItems = document.querySelectorAll('.accordion__item');
 let accordionText = document.querySelectorAll('.accordion-text');
+let arrows = document.querySelectorAll('.arrow');
 let currentActive = null;
 
 for (let i = 0; i < accordionItems.length; i++) {
     accordionItems[i].addEventListener(('click'), function () {
         if (currentActive === null) {
             accordionText[i].classList.add("show");
+            arrows[i].classList.add("arrow-rotate");
             currentActive = i;
         } else if (currentActive === i) {
             accordionText[currentActive].classList.remove("show");
+            arrows[i].classList.remove("arrow-rotate");
             currentActive = null;
         } else {
             accordionText[currentActive].classList.remove("show");
+            arrows[currentActive].classList.remove("arrow-rotate");
             accordionText[i].classList.add("show");
+            arrows[i].classList.add("arrow-rotate");
             currentActive = i;
         }
     });
