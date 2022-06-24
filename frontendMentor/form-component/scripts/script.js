542c2bd0d8cb026549259364c469d1738591269e
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-24 16:51:40 +0200

    Form formatting

--- frontendMentor/form-component/scripts/script.js
+++ frontendMentor/form-component/scripts/script.js
@@ -1,3 +1,7 @@
 // Receive an error message when the form is submitted if:
 // Any input field is empty
 // The email address is not formatted correctly
+
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Advanced_form_styling
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/UI_pseudo-classes
