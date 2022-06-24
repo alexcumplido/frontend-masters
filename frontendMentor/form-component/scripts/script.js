e49157d0529500b024a979c575a0de328ffdbe46
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-24 19:49:21 +0200

    Clean and formatted js file

--- frontendMentor/form-component/scripts/script.js
+++ frontendMentor/form-component/scripts/script.js
@@ -11,26 +11,7 @@ const inputErrors = document.querySelectorAll('.span-error');
 const submitButton = document.querySelector('.btn-submit');
 const form = document.querySelector('form');
 
-
-// Input
-// Value of HTML input(variables referencing HTML inputs)
-
-// Manipulation
-// Create flow structure were inputs ara tested for regEx match
-
-// Output
-// Success message if match
-// Esrror message if not match
-
-
-form.addEventListener('submit', function (event) {
-    debugger;
-    if (!validation()) {
-        event.preventDefault();
-    }
-})
-
-function validation() {
+function validateInputs() {
     debugger;
     let validation = false;
     validation = (regexFullName.test(inputFirstName.value)) ? true : false;
@@ -39,6 +20,13 @@ function validation() {
     validation = (regexEmail.test(inputPassword.value)) ? true : false;
     return validation;
 }
+
+form.addEventListener('submit', function (event) {
+    if (!validateInputs()) {
+        event.preventDefault();
+    }
+})
+
 
 
 // Add eventListeners to stored inputs
