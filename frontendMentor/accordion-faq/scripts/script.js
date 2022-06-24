6123483feec3dd7186426689c82ace50f88b07af
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-24 12:21:16 +0200

    Final layout

--- frontendMentor/accordion-faq/scripts/script.js
+++ frontendMentor/accordion-faq/scripts/script.js
@@ -1,21 +1,26 @@
-let accordionItems = document.querySelectorAll('.accordion__item');
-let accordionText = document.querySelectorAll('.accordion-text');
-let arrows = document.querySelectorAll('.arrow');
+const accordionItems = document.querySelectorAll('.accordion__item');
+const accordionTitles = document.querySelectorAll(".accordion-title");
+const accordionText = document.querySelectorAll('.accordion-text');
+const arrows = document.querySelectorAll('.arrow');
 let currentActive = null;
 
 for (let i = 0; i < accordionItems.length; i++) {
     accordionItems[i].addEventListener(('click'), function () {
         if (currentActive === null) {
+            accordionTitles[i].classList.add("title-active");
             accordionText[i].classList.add("show");
             arrows[i].classList.add("arrow-rotate");
             currentActive = i;
         } else if (currentActive === i) {
+            accordionTitles[i].classList.remove("title-active");
             accordionText[currentActive].classList.remove("show");
             arrows[i].classList.remove("arrow-rotate");
             currentActive = null;
         } else {
+            accordionTitles[currentActive].classList.remove("title-active");
             accordionText[currentActive].classList.remove("show");
             arrows[currentActive].classList.remove("arrow-rotate");
+            accordionTitles[i].classList.add("title-active");
             accordionText[i].classList.add("show");
             arrows[i].classList.add("arrow-rotate");
             currentActive = i;
