07a7da94f78452b18f8828862c9cae08677727c4
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-25 12:51:33 +0200

    Constraint validation in progress

--- frontendMentor/form-component/scripts/script.js
+++ frontendMentor/form-component/scripts/script.js
@@ -3,22 +3,58 @@ const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const regexPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 //regexPass 8 characters, at least 1 letter and 1 number:
 
-const inputFirstName = document.getElementById('firstName');
-const inputLastName = document.getElementById('lastName');
-const inputEmail = document.getElementById('emailAdress');
-const inputPassword = document.getElementById('password');
-const inputErrors = document.querySelectorAll('.span-error');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const password = document.getElementById('password');
+
+
+const emailAdress = document.getElementById('emailAdress');
+const spanError = document.querySelector('.error-emailAdress');
 const submitButton = document.querySelector('.btn-submit');
 const form = document.querySelector('form');
 
-const spanError = document.querySelector('.error');
+emailAdress.addEventListener("input", function (event) {
+    if (emailAdress.validity.valid) {
+        spanError.textContent = ''; // Reset the content of the message
+        spanError.className = 'error'; // Reset the visual state of the ;
+    } else {
+        showError();
+    }
+})
+
+form.addEventListener('submit', function (event) {
+    if (!emailAdress.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+});
+
+function showError() {
+    if (emailAdress.validity.valueMissing) {
+        spanError.textContent = 'You need to enter an e-mail address.';
+    } else if (emailAdress.validity.typeMismatch) {
+        spanError.textContent = 'Entered value needs to be an e-mail address.';
+    } else if (emailAdress.validity.tooShort) {
+        spanError.textContent = `Email should be at least ${emailAdress.minLength} characters; you entered ${emailAdress.value.length}.`;
+    }
+    spanError.className = 'error-emailAdress active';
+}
+
+
+
+
+
+
+
+
+
 
 // function validateInputs() {
 //     let validation = false;
-//     validation = (regexFullName.test(inputFirstName.value)) ? true : false;
-//     validation = (regexFullName.test(inputLastName.value)) ? true : false;
-//     validation = (regexEmail.test(inputEmail.value)) ? true : false;
-//     validation = (regexPass.test(inputPassword.value)) ? true : false;
+//     validation = (regexFullName.test(firstName.value)) ? true : false;
+//     validation = (regexFullName.test(lastName.value)) ? true : false;
+//     validation = (regexEmail.test(emailAdress.value)) ? true : false;
+//     validation = (regexPass.test(password.value)) ? true : false;
 //     return validation;
 // }
 
@@ -27,7 +63,6 @@ const spanError = document.querySelector('.error');
 //         event.preventDefault();
 //     }
 // })
-
 
 // Add eventListeners to stored inputs
 // Prevent default behaviour of submitted button
