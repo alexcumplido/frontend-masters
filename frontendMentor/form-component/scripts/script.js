0383bcf4dcc7af0b7e45f3a7f0a5d7c3c342ac82
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-26 17:11:08 +0200

    Validation implemented

--- frontendMentor/form-component/scripts/script.js
+++ frontendMentor/form-component/scripts/script.js
@@ -1,73 +1,94 @@
-const regexFullName = /^[a-z ,.'-]+$/i;
+const regexFirstName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+const regexLastName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
 const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const regexPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-//regexPass 8 characters, at least 1 letter and 1 number:
+
+const form = document.querySelector('.form');
 
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
+const email = document.getElementById('emailAdress');
 const password = document.getElementById('password');
 
-
-const emailAdress = document.getElementById('emailAdress');
-const spanError = document.querySelector('.error-emailAdress');
-const submitButton = document.querySelector('.btn-submit');
-const form = document.querySelector('form');
-
-emailAdress.addEventListener("input", function (event) {
-    if (emailAdress.validity.valid) {
-        spanError.textContent = ''; // Reset the content of the message
-        spanError.className = 'error'; // Reset the visual state of the ;
-    } else {
-        showError();
-    }
-})
+const errorFirstName = document.querySelector('.errorFirstName');
+const errorLastName = document.querySelector('.errorLastName');
+const errorEmail = document.querySelector('.errorEmail');
+const errorPassword = document.querySelector('.errorPassword');
+// regexPass 8 characters, at least 1 letter and 1 number:
 
 form.addEventListener('submit', function (event) {
-    if (!emailAdress.validity.valid) {
-        showError();
-        event.preventDefault();
-    }
+    if (validateFirstName() === false) event.preventDefault();
+    if (validateLastName() === false) event.preventDefault();
+    if (validateEmail() === false) event.preventDefault();
+    if (validatePassword() === false) event.preventDefault();
 });
 
-function showError() {
-    if (emailAdress.validity.valueMissing) {
-        spanError.textContent = 'You need to enter an e-mail address.';
-    } else if (emailAdress.validity.typeMismatch) {
-        spanError.textContent = 'Entered value needs to be an e-mail address.';
-    } else if (emailAdress.validity.tooShort) {
-        spanError.textContent = `Email should be at least ${emailAdress.minLength} characters; you entered ${emailAdress.value.length}.`;
+function validateFirstName() {
+    let validate = true;
+    if (firstName.value === "") {
+        validate = false;
+        errorFirstName.textContent = 'First Name can not be empty.';
+        errorFirstName.classList.add('active');
+    } else if (regexFirstName.test(firstName.value) === false) {
+
+        validate = false;
+        errorFirstName.textContent = 'First Name can not containt numeric or special values.';
+        errorFirstName.classList.add('active');
+    } else {
+        errorFirstName.textContent = '';
+        errorFirstName.classList.remove('active');
     }
-    spanError.className = 'error-emailAdress active';
+    return validate;
 }
 
+function validateLastName() {
+    let validate = true;
+    if (lastName.value === "") {
+        validate = false;
+        errorLastName.textContent = 'Last Name can not be empty.';
+        errorLastName.classList.add('active');
+    } else if (regexLastName.test(lastName.value) === false) {
 
+        validate = false;
+        errorLastName.textContent = 'Last Name can not containt numeric or special values.';
+        errorLastName.classList.add('active');
+    } else {
+        errorLastName.textContent = '';
+        errorLastName.classList.remove('active');
+    }
+    return validate;
+}
 
+function validateEmail() {
+    let validate = true;
+    if (email.value === "") {
+        validate = false;
+        errorEmail.textContent = 'Email can not be empty.';
+        errorEmail.classList.add('active');
+    } else if (regexEmail.test(email.value) === false) {
+        validate = false;
+        errorEmail.textContent = 'Email need to be a valid email.';
+        errorEmail.classList.add('active');
+    } else {
+        errorEmail.textContent = '';
+        errorEmail.classList.remove('active');
+    }
+    return validate;
+}
 
-
-
-
-
-
-
-// function validateInputs() {
-//     let validation = false;
-//     validation = (regexFullName.test(firstName.value)) ? true : false;
-//     validation = (regexFullName.test(lastName.value)) ? true : false;
-//     validation = (regexEmail.test(emailAdress.value)) ? true : false;
-//     validation = (regexPass.test(password.value)) ? true : false;
-//     return validation;
-// }
-
-// form.addEventListener('submit', function (event) {
-//     if (!validateInputs()) {
-//         event.preventDefault();
-//     }
-// })
-
-// Add eventListeners to stored inputs
-// Prevent default behaviour of submitted button
-// if user input validation mismatch
-// if mismatch show error message in certain inputs
-// if user input does not mismatch proceed with submitting
-//         if does not mismatch show success message
-
+function validatePassword() {
+    let validate = true;
+    if (password.value === "") {
+        validate = false;
+        errorPassword.textContent = 'Password can not be empty.';
+        errorPassword.classList.add('active');
+    } else if (regexPass.test(password.value) === false) {
+        validate = false;
+        errorPassword.textContent = 'Password need to be a valid password.';
+        errorPassword.classList.add('active');
+    } else {
+        errorPassword.textContent = '';
+        errorPassword.classList.remove('active');
+    }
+    return validate;
+}
