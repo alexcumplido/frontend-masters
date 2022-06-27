c0826ec83988b707e7ce006c6ac5832dfc5173c1
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-27 11:01:45 +0200

    Basic styles applied

---
+++ frontendMentor/chart-component/scripts/script.js
@@ -0,0 +1,9 @@
+// We provide the data for the chart in a local`data.json` file.So you can use that to dynamically add the bars if you choose.
+
+// Your users should be able to:
+
+// - View the bar chart and hover over the individual bars to see the correct amounts for each day
+// - See the current day's bar highlighted in a different colour to the other bars
+// - View the optimal layout for the content depending on their device's screen size
+// - See hover states for all interactive elements on the page
+// - ** Bonus **: See dynamically generated bars based on the data provided in the local JSON file
