f95036a8f007868af14e5c7103d996735ea7696a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-27 11:25:17 +0200

    Chart.js installed

--- frontendMentor/chart-component/scripts/script.js
+++ frontendMentor/chart-component/scripts/script.js
@@ -7,3 +7,45 @@
 // - View the optimal layout for the content depending on their device's screen size
 // - See hover states for all interactive elements on the page
 // - ** Bonus **: See dynamically generated bars based on the data provided in the local JSON file
+
+// Understand how Chart.js works
+// Create a sample example
+
+// 
+
+const ctx = document.getElementById('myChart');
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
