823e77e4cf6da6341cc695066539fd0de48d8b75
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-06-27 16:06:57 +0200

    Fetched local json via then

--- frontendMentor/chart-component/scripts/script.js
+++ frontendMentor/chart-component/scripts/script.js
@@ -9,27 +9,55 @@
 // Output data
 
 
-//See dynamically generated bars based on the data provided in the local JSON file
+// INPUT
+// Data from JSON file
 
-// User see each bar representing associated data
-// User hover over the individual bar an see associated
+// MANIPULATION
+// Acess JSON object
+
+// OUTPUT
+// dynamically generated bars based on the data provided in the local JSON file
+// User hover over the individual bar and see its data
 // User see current day's bar in a different colour
 
+// Create variables
+// variable for array of objects from localJson
+// variables that selects bars class
+// variable that selects days class
+// variable that selects bar-wrapper element
+// variable that selects day-wrapper element
+// function that receives and transform data from JSON
+
+// function that iterates over jsontransformed and extract values
+// function that creates elements representing bars
+// function that creates elements representing days
+
+
 // Import local JSON file
-// Selection and creation of elements in the DOM
+// Iterate over data and in each iteration
+// Append two elements in corresponding wrappers
+// On bar-wrapper append:
+//In each creation style the width according data add px as height
+// On day-wrapper append:
+//On each creation assign corresponding day
 
-// Receive data from JSON
-// For each object create two elements
-// One for day
-// One for amount
 
-// Apped
+const urlRequest = "./json/data.json";
+let responseFetched;
 
-// If day matches with current day add special styles to day element
-// Add width in px 
+async function fetchResponse() {
 
-function init() {
-    console.log('Document loaded')
+    fetch(urlRequest)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (processedResponse) {
+            responseFetched = JSON.parse(processedResponse);
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+    return responseFetched;
+
 }
-
-document.addEventListener('DOMContentLoaded', init);
+fetchResponse();
