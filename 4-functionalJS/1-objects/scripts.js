d08baa8f99dd57f7d0f19bbf81994a340dcb7f8c
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-07 12:11:09 +0200

    Rename course folders

---
+++ 4-functionalJS/1-objects/scripts.js
@@ -0,0 +1,129 @@
+// Objects
+var personObject = {};
+personObject.name = "Mrs. White";
+var who = personObject.name; // Storing a value by value
+personObject.name = "Mr. White";
+// Storing a value by reference 
+// The value of "Mrs. White" goes garbage collected since "Mr. White is the new value referenced for the property perosn.name"
+who.story // Undefined 
+
+// Arrays
+var personArray = [];
+personArray.name = "Mrs. White";
+var who = personArray.name; // Because arrays are objects we can set properties inside of them
+let typeArray = (typeof personArray === "array");
+let typeObject = (typeof personArray === "object");
+let isArray = Array.isArray(personArray);
+let objectKeys = Object.keys(personArray);
+
+personArray[0] = "I was not in ..."; // 0 is being stringify
+try {
+    personArray[statement] = "I would never!" // JS parses this line and does not find an string value for plea
+} catch {
+    console.log(typeof statement)
+}
+var plea = "wouldShe";
+personArray[plea] = "I would never";
+//JS check for varialble plea in the scope and get the variable plea that holds a copy of the string value "wouldShee"
+//A new property is set with key "wouldShea" and value "I would never"
+
+
+// Object array Rules
+// Array is a type of object with attached methods
+// Before accessing a property have to be defined
+// Dot notation coerces into a string when bracket notation does not
+// Dot does not work with number or unusual characters, only with values type string or that can be coercet to it
+
+// The main difference between arrays and others objects is that an array has the property .length
+// wich is computed as you add numerical indexes
+// and numerical idexes are different than properties on an array because an array captures that and increment the length
+
+// Create an object an array that represents a clue game
+// Values: Characters, weapons, rooms, special card with character, room, weapon
+
+var game = {};
+
+game['weapons'] = [
+    {
+        type: 'lasers',
+        location: 'lab'
+    },
+    {
+        type: 'angry cats',
+        location: 'entrance'
+    },
+    {
+        type: 'dish soap',
+        location: 'kitchen'
+    },
+
+]
+
+game.name = []
+game.name[0] = "Bianca";
+game.name[1] = "Marc";
+game.name[2] = "Brian";
+
+// What is an object ?
+// A non primitive value that can hold any type of value and has methods attached to it.
+// Store data in a syntax of properties and values, "keys" = "values".
+
+
+// What is the difference between dot and bracket notation ?
+// Dot notationa accepts only a string type value and save us tipping time.
+
+// How do you add a property with a key that contains special characters ?
+// Through bracket notation wrapper in question marks
+
+// How do you add a property whose key and value are stored in different variables ?
+// Using dot notation in case is a pure string or bracket notation
+
+// object[property] = value;
+
+// How do we loop through objects to access the values ?
+for (const key in object) {
+    console.log(`${key}: ${user.key}`)
+}
+
+
+
+let object = {
+    first: 0, second: 1
+}
+
+// let { second, first } = object;
+
+
+const confidential = {
+    name: "Rusty",
+    room: "kitchen",
+    weapon: "candlestick"
+}
+
+confidential.room;
+console.log(confidential.weapon)
+
+let { room, weapon } = confidential;
+
+//Destructuring an array
+let [firstNumber, secondNumber] = [1, 2];
+//=> 1 2
+
+//Omit certain values
+let [one, , three] = [1, 2, 3]
+//=> 1 3
+
+// Combine with spread/rest operator (accumulates the rest of the values)
+let [first, ...others] = [1, 2, 3]
+//=> 1 [2,3]
+
+// Swap variables easily without a temporary variables
+// Before
+var a = 1, b = 2;
+console.log(a, b)
+// var temporary = a;
+// a = b;
+// b = temporary;
+
+console.log([b, a] = [a, b])
+
