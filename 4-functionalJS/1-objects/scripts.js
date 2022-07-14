6584476f51a106f438b5813934cefc415bed08a3
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-07-14 09:19:19 +0200

    Review object notes

--- 4-functionalJS/1-objects/scripts.js
+++ 4-functionalJS/1-objects/scripts.js
@@ -3,17 +3,16 @@
     var personObject = {};
     personObject.name = "Mrs. White";
     var who = personObject.name; // Storing a value by value
-    personObject.name = "Mr. White";
-    // Storing a value by reference 
-    // The value of "Mrs. White" goes garbage collected since "Mr. White is the new value referenced for the property perosn.name"
+    personObject.name = "Mr. White"; // Storing a value by reference 
+    // The value of "Mrs. White" goes garbage collected since "Mr. White is the new value referenced for the property person.name"
     who.story // Undefined 
 
     // Arrays
     var personArray = [];
     personArray.name = "Mrs. White";
     var who = personArray.name; // Because arrays are objects we can set properties inside of them
-    let typeArray = (typeof personArray === "array");
-    let typeObject = (typeof personArray === "object");
+    let typeArray = (typeof personArray === "array"); // false
+    let typeObject = (typeof personArray === "object"); // true
     let isArray = Array.isArray(personArray);
     let objectKeys = Object.keys(personArray);
 
@@ -67,11 +66,20 @@
     game.name[2] = "Brian";
 }
 
+{
+    //Prevention of object mutation via Object.freeze
+    let objectFreezed = {
+        prop1: "Alex",
+        prop2: "Laura"
+    }
+
+    Object.freeze(objectFreezed);
+    objectFreezed.prop1 = "Alexandre";
+}
 
 // What is an object ?
 // A non primitive value that can hold any type of value and has methods attached to it.
-// Store data in a syntax of properties and values, "keys" = "values".
-
+// Store data in a syntax of properties and values, "keys" : "values".
 
 // What is the difference between dot and bracket notation ?
 // Dot notationa accepts only a string type value and save us tipping time.
@@ -88,38 +96,30 @@
 // ES6 Destructuring
 // Declare a variable and reference to them
 
-const [first, second, third] = [true, false]
-
-
-let objectFreezed = {
-    prop1: "Alex",
-    prop2: "Laura"
-}
-
-Object.freeze(objectFreezed);
-objectFreezed.prop1 = "Alexandre";
-
-
-
-let confidential = {
-    name: "Rusty",
-    room: "kitchen",
-    weapon: "candlestick",
-}
-{
-    //Destructuring save us time
-    let { weapon, name, room } = confidential;
-}
+const [first, second, third] = [true, false];
 
 {
+    let confidential = {
+        name: "Rusty",
+        room: "kitchen",
+        weapon: "candlestick",
+    }
     let name = confidential.name;
     let room = confidential.room;
     let weapon = confidential.weapon;
-}
 
-//How to iterate over an object keys ? 
-for (const key in confidential) {
-    console.log(`${key}: ${confidential.key}`)
+    {
+        //Destructuring save us time
+        let { weapon, name, room } = confidential;
+    }
+
+    {
+        //How to iterate over an object keys ?
+        for (const key in confidential) {
+            console.log(`${key}: ${confidential.key}`)
+        }
+    }
+
 }
 
 //Destructuring an array
@@ -143,18 +143,17 @@ for (const key in confidential) {
 {
     // Swap variables easily without a temporary variables
     // Before
-    let a = 1, b = 2;
+    let a = 1
+    let b = 2;
     [b, a] = [a, b];
     console.log(`Now: a: ${a}, b: ${b}`)
 }
 
 {
-    let a = 1, b = 2;
+    let a = 1
+    let b = 2;
     let temporary = a;
     a = b;
     b = temporary;
-    console.log([b, a] = [a, b])
-
+    console.log(a, b);
 }
-
-
