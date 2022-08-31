beab73a3f3f374828e64dc5c4fb5c502d1b330e2
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-08-31 19:22:37 +0200

    Review callback lectures

--- 5-javaScritpHardParts/1-Callbacks/script.js
+++ 5-javaScritpHardParts/1-Callbacks/script.js
@@ -60,46 +60,4 @@ let resultAdd = copyArrayAndManipulate(array, addBy2);
 
 
 // http://csbin.io/callbacks
-// In challenge 3, you've created a function called map. In this challenge, you're going to rebuild the map function by creating a function called mapWith.This time you're going to use forEach inside of mapWith instead of using a for loop.
-
-//Challenge 3
-function map(array, callback) {
-    let storage = [];
-    if (Array.isArray(array)) {
-        for (let i = 0; i < array.length; i++) {
-            storage.push(callback(array[i], i, array));
-        }
-    } else {
-        for (let key in array) {
-            storage.push(callback(array[key], key, array));
-        }
-    }
-    return storage;
-}
-
-//Challenge 4
-function forEach(array, callback) {
-    if (Array.isArray(array)) {
-        for (let i = 0; i < array.length; i++) {
-            callback(array[i], i, array);
-        }
-    } else {
-        for (let key in array) {
-            callback(array[key], key, array);
-        }
-    }
-}
-
-//Challenge 5
-function mapWith(array, callback) {
-    let storage = [];
-    forEach(array, function () {
-        storage.push(callback);
-    });
-    return storage;
-}
-
-let arrayNumbers = [1, 2, 3];
-let testing = mapWith(arrayNumbers, function (element, index, list) {
-    return element + 1;
-});
+// Challenge 1,2,3,4,5
