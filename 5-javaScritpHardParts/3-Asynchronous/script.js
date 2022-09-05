e93b6242a7f73a169019c0e66d313f05ccdfd7cc
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-09-05 18:27:00 +0200

    Asynchronous challenges

--- 5-javaScritpHardParts/3-Asynchronous/script.js
+++ 5-javaScritpHardParts/3-Asynchronous/script.js
@@ -1,15 +1,81 @@
 
-// Exercises http://csbin.io/async || http://csbin.io/promises
+{
+    function printName() {
+        console.log('Hey frontend developer!')
+    }
+    setTimeout(printName, 0);
+    for (let i = 0; i < 5000; i++) {
+        console.log(i);
+    }
 
-function printName() {
-    console.log('Hey frontend developer!')
+    console.log('Me First');
 }
 
-debugger;
-setTimeout(printName, 4000);
+// Exercises http://csbin.io/async
 
-for (let i = 0; i < 5000; i++) {
-    console.log(i);
+{
+    /* CHALLENGE 1 */
+    function sayHowdy() {
+        console.log('Howdy');
+    }
+    function testMe() {
+        setTimeout(sayHowdy, 0);
+        console.log('Partnah');
+    }
+    testMe(); // Partnah first then Howdy
 }
 
-console.log('Me First');
+{
+    /* CHALLENGE 2 */
+    function sayWelcome() {
+        console.log('welcome')
+    }
+    function delayedGreet() {
+        setTimeout(sayWelcome, 3000);
+    }
+    delayedGreet(); // should log (after 3 seconds): welcome
+}
+
+{
+    /* CHALLENGE 3 */
+    function sayGoodBye() {
+        console.log(`good bye`)
+    }
+    function helloGoodbye() {
+        console.log(`hello`);
+        setTimeout(sayGoodBye, 3000);
+    }
+    helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+}
+
+{
+    /* CHALLENGE 4 */
+    let intervalId;
+    function clearAsync() {
+        clearInterval(intervalId);
+    }
+    function brokenRecord() {
+        intervalId = setInterval(sayWelcome, 2000);
+        setTimeout(clearAsync, 10000);
+    }
+    brokenRecord(); // should log (every second): hi again
+}
+
+{
+    /* CHALLENGE 5 */
+    let intervalId;
+    function sayHi() {
+        console.log(`hi for now`)
+    }
+    function clearAsync() {
+        clearInterval(intervalId);
+    }
+    function limitedRepeat() {
+        intervalId = setInterval(sayHi, 1000);
+        setTimeout(clearAsync, 5000);
+    }
+    // Uncomment the following line to check your work!
+    limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+}
+
+// Exercises http://csbin.io/promises
