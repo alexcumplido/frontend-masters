d639e01a9ab7a0ab4aa3c8bea921d32872b54f78
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-31 18:29:05 +0100

    Updated destructuring examples

--- 4-functionalJS/1-objects/script.js
+++ 4-functionalJS/1-objects/script.js
@@ -137,3 +137,31 @@ const [first, second, third] = [true, false];
     a = b;
     b = temporary;
 }
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+
+function nestedArrayAndObject() {
+  // refactor this to a single line of destructuring...
+  const info = {
+    title: 'Once Upon a Time',
+    protagonist: {
+      name: 'Emma Swan',
+      enemies: [
+        {name: 'Regina Mills', title: 'Evil Queen'},
+        {name: 'Cora Mills', title: 'Queen of Hearts'},
+        {name: 'Peter Pan', title: `The boy who wouldn't grow up`},
+        {name: 'Zelena', title: 'The Wicked Witch'},
+      ],
+    },
+  }
+  // const {} = info // <-- replace the next few `const` lines with this
+  const title = info.title
+  const protagonistName = info.protagonist.name
+  const enemy = info.protagonist.enemies[3]
+  const enemyTitle = enemy.title
+  const enemyName = enemy.name
+  return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`
+}
