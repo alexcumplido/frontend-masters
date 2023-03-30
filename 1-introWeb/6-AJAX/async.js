fc6056a45ac28f82211f113cf2c47b1f69e6e350
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-03-30 17:13:40 +0200

    Add async examples

--- 1-introWeb/6-AJAX/async.js
+++ 1-introWeb/6-AJAX/async.js
@@ -1,41 +1,4 @@
 {
-    const dogTarget = document.querySelector('.dog-target');
-    const buttonDog = document.querySelector('.dog-btn');
-
-    const DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
-    function fetchDog() {
-        fetch(DOG_URL)
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (processedResponse) {
-                const dogObj = JSON.parse(processedResponse);
-                const img = document.createElement('img');
-                img.src = dogObj.message;
-                img.alt = "Cute doggo";
-                dogTarget.appendChild(img);
-            })
-            .catch((function (error) {
-                dogTarget.innerText = error;
-            }));
-    }
-
-    async function fetchDog() {
-        const promise = await fetch(DOG_URL);
-        const processedResponse = await promise.json();
-        const img = document.createElement('img');
-        img.src = processedResponse.message;
-        img.alt = "Cute doggo";
-        dogTarget.appendChild(img);
-    }
-
-    buttonDog.addEventListener('click', function (event) {
-        fetchDog();
-    });
-}
-
-{
     function houseOne(){
         console.log(`Paper delivered to house 1`);
     }
@@ -44,7 +7,7 @@
         setTimeout(() => {
             console.log(`Paper delivered to house 2`);
             callback();
-        }, 3000)
+        }, 0)
     }
 
     function houseThree(){
@@ -145,5 +108,41 @@
     }
 
     getACuteDogPhoto();
+}
+
+{
+    const dogTarget = document.querySelector('.dog-target');
+    const buttonDog = document.querySelector('.dog-btn');
+    const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+    function fetchDog() {
+        fetch(DOG_URL)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (processedResponse) {
+                const dogObj = JSON.parse(processedResponse);
+                const img = document.createElement('img');
+                img.src = dogObj.message;
+                img.alt = "Cute doggo";
+                dogTarget.appendChild(img);
+            })
+            .catch((function (error) {
+                dogTarget.innerText = error;
+            }));
+    }
+
+    async function fetchDog() {
+        const promise = await fetch(DOG_URL);
+        const processedResponse = await promise.json();
+        const img = document.createElement('img');
+        img.src = processedResponse.message;
+        img.alt = "Cute doggo";
+        dogTarget.appendChild(img);
+    }
+
+    buttonDog.addEventListener('click', function (event) {
+        fetchDog();
+    });
 }
 
